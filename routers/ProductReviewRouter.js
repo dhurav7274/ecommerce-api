f535f5e6b1101e0a5d70e96362b7eb8ff8d31f5a
DHURAV KUMAR <103018791+dhurav7274@users.noreply.github.com>
2024-12-26 17:09:15 +0530

    inital commit

--- routers/ProductReviewRouter.js
+++ routers/ProductReviewRouter.js
@@ -7,15 +7,15 @@ import {
 } from "../controllers/ProductReviewController.js";
 import { authenticateUser } from "../middlewares/AuthenticateMiddleware.js";
 
-const router = express.Router();
+const ProductReviewRouter = express.Router();
 
 // Add a review to a product
-router.post("/", authenticateUser, addReview);
+ProductReviewRouter.post("/", authenticateUser, addReview);
 
 // Get all reviews for a product
-router.get("/:productId", getProductReviews);
+ProductReviewRouter.get("/:productId", getProductReviews);
 
 // Delete a review
-router.delete("/:reviewId", authenticateUser, deleteReview);
+ProductReviewRouter.delete("/:reviewId", authenticateUser, deleteReview);
 
 export { router };
